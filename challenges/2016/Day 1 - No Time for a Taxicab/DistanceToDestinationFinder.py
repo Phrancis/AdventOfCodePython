016c594e01036dec51b5f502e4323d476d974b7c
Phrancis <Phrancis@users.noreply.github.com>
2018-06-09 14:59:01 -0400

    Implement most of the solution to part 2

--- challenges/2016/Day 1 - No Time for a Taxicab/DistanceToDestinationFinder.py
+++ challenges/2016/Day 1 - No Time for a Taxicab/DistanceToDestinationFinder.py
@@ -1,12 +1,28 @@
-"""
-Advent of Code - Day 1: No time for a Taxicab
-http://adventofcode.com/2016/day/1
-"""
+from Position import *
 
 class DistanceToDestinationFinder:
+
+    """
+    Advent of Code - Day 1: No time for a Taxicab
+    http://adventofcode.com/2016/day/1
+
+    Solves the given problem, parts 1 & 2, in a quite literal implementation
+    of taxicab geometry, deriving distances and positions based on movement
+    in the 4 cardinal points.
+    """
+    
     CARDINALS_CLOCKWISE = ["N", "E", "S", "W"]
     
-    def __init__(self, raw_directions:str) -> None:
+    def __init__(self, raw_directions:str) -> "DistanceToDestinationFinder":
+        """
+        Constructor.
+        - Parses raw_directions into a List
+        - Sets starting current_direction to North
+        - Creates a Dict to log how many steps in each cardinal direction
+          we have taken from the origin point
+        - Creates empty position_history List
+        - Adds starting position to position_history list
+        """
         self.directions = raw_directions.split(", ")
         self.current_direction = "N"
         self.moves_in_each_direction = {
@@ -14,6 +30,8 @@ class DistanceToDestinationFinder:
             "E" : 0,
             "S" : 0,
             "W" : 0 }
+        self.position_history = []
+        self.position_history.append(Position(0,0))
 
     def get_current_direction_index(self) -> int:
         """Return numeric index of current direction."""
@@ -35,7 +53,7 @@ class DistanceToDestinationFinder:
 
     def apply_turn_move_instruction(self, instruction:str) -> None:
         """
-        Takes a string like 'R1' or 'L2' and updates state to turn and move
+        Takes a string like 'R1' or 'L200' and updates state to turn and move
         accordingly, e.g., 'R1' = turn right and move 1 space.
         """
         turn = instruction[0].upper()
@@ -51,8 +69,29 @@ class DistanceToDestinationFinder:
         else:
             raise ValueError(f"Invalid turn instruction: {turn}")
         self.moves_in_each_direction[self.current_direction] += moves
+        self.position_history.append(self.get_current_position())
 
-    def get_shortest_path_using_whole_input(self) -> int:
+    def get_current_position(self) -> "Position":
+        """
+        Calculates and returns the current position relative to the
+        starting point.
+        """
+        north = self.moves_in_each_direction["N"]
+        south = self.moves_in_each_direction["S"]
+        east = self.moves_in_each_direction["E"]
+        west = self.moves_in_each_direction["W"]
+        latitude = north - south
+        longitude = east - west
+        return Position(latitude, longitude)
+
+    def position_has_already_been_visited(self, other_position:"Position") -> bool:
+        """Returns whether a given position has already been visited."""
+        for position in self.position_history:
+            if position == other_position:
+                return True
+        return False
+
+    def get_shortest_path_from_input(self) -> int:
         """
         Solves Part 1 of the puzzle, which is finding the shortest distance
         with the entire input string regardless of locations. 
@@ -67,7 +106,15 @@ class DistanceToDestinationFinder:
         longitude_movement = abs(east - west)
         return latitude_movement + longitude_movement
 
+    def get_human_friendly_position_history(self) -> str:
+        """Returns a human-friendly position history."""
+        human_readable_position_history = ""
+        for i in range(len(self.position_history)):
+            human_readable_position_history += f"position_id:{i}, position:({str(self.position_history[i])})\n"
+        return human_readable_position_history
+
 if __name__ == "__main__":
     puzzle_input = "R3, L2, L2, R4, L1, R2, R3, R4, L2, R4, L2, L5, L1, R5, R2, R2, L1, R4, R1, L5, L3, R4, R3, R1, L1, L5, L4, L2, R5, L3, L4, R3, R1, L3, R1, L3, R3, L4, R2, R5, L190, R2, L3, R47, R4, L3, R78, L1, R3, R190, R4, L3, R4, R2, R5, R3, R4, R3, L1, L4, R3, L4, R1, L4, L5, R3, L3, L4, R1, R2, L4, L3, R3, R3, L2, L5, R1, L4, L1, R5, L5, R1, R5, L4, R2, L2, R1, L5, L4, R4, R4, R3, R2, R3, L1, R4, R5, L2, L5, L4, L1, R4, L4, R4, L4, R1, R5, L1, R1, L5, R5, R1, R1, L3, L1, R4, L1, L4, L4, L3, R1, R4, R1, R1, R2, L5, L2, R4, L1, R3, L5, L2, R5, L4, R5, L5, R3, R4, L3, L3, L2, R2, L5, L5, R3, R4, R3, R4, R3, R1"
-    shortest_path_finder = DistanceToDestinationFinder(puzzle_input)
-    print(shortest_path_finder.get_shortest_path_using_whole_input())
+    finder = DistanceToDestinationFinder(puzzle_input)
+    print(finder.get_shortest_path_from_input())
+    print(finder.get_human_friendly_position_history())
--- challenges/2016/Day 1 - No Time for a Taxicab/TestDistanceToDestinationFinder.py
+++ challenges/2016/Day 1 - No Time for a Taxicab/TestDistanceToDestinationFinder.py
@@ -1,6 +1,7 @@
 import unittest
 from typing import List
 from DistanceToDestinationFinder import *
+from Position import *
 
 class TestDistanceToDestinationFinder(unittest.TestCase):
     def test_directions_are_converted_to_list(self):
@@ -12,6 +13,10 @@ class TestDistanceToDestinationFinder(unittest.TestCase):
     def test_facing_north_initially(self):
         finder = DistanceToDestinationFinder("")
         self.assertEqual("N", finder.current_direction)
+
+    def test_initial_position_is_correct(self):
+        finder = DistanceToDestinationFinder("")
+        self.assertEqual(Position(0,0), finder.position_history[0])
 
     def test_get_current_direction_index_for_all_cardinals(self):
         finder = DistanceToDestinationFinder("")
@@ -64,25 +69,43 @@ class TestDistanceToDestinationFinder(unittest.TestCase):
         finder.apply_turn_move_instruction("L1000")
         self.assertEqual(1000, finder.moves_in_each_direction["S"])  
 
+    def test_get_current_position(self):
+        finder = DistanceToDestinationFinder("")
+        self.assertEqual(Position(0,0), finder.get_current_position())
+        finder.apply_turn_move_instruction("R1")
+        self.assertEqual(Position(0,1), finder.get_current_position())
+        finder.apply_turn_move_instruction("R1")
+        self.assertEqual(Position(-1,1), finder.get_current_position())
+        finder.apply_turn_move_instruction("R1")
+        self.assertEqual(Position(-1,0), finder.get_current_position())
+        finder.apply_turn_move_instruction("R1")
+        self.assertEqual(Position(0,0), finder.get_current_position())
+
+    def test_position_has_already_been_visited(self):
+        finder = DistanceToDestinationFinder("")
+        finder.position_history.append(Position(1,1))
+        finder.position_history.append(Position(2,2))
+        self.assertTrue(finder.position_has_already_been_visited(Position(1,1)))
+
     def test_part1_R2_L3(self):
         test_input = "R2, L3"
         finder = DistanceToDestinationFinder(test_input)
-        self.assertEqual(5, finder.get_shortest_path_using_whole_input())
+        self.assertEqual(5, finder.get_shortest_path_from_input())
         
     def test_part1_R2_R2_R2(self):
         test_input = "R2, R2, R2"
         finder = DistanceToDestinationFinder(test_input)
-        self.assertEqual(2, finder.get_shortest_path_using_whole_input())
+        self.assertEqual(2, finder.get_shortest_path_from_input())
 
     def test_part1_R5_L5_R5_R3(self):
         test_input = "R5, L5, R5, R3"
         finder = DistanceToDestinationFinder(test_input)
-        self.assertEqual(12, finder.get_shortest_path_using_whole_input())
+        self.assertEqual(12, finder.get_shortest_path_from_input())
 
     def test_part1_with_puzzle_input(self):
         test_input = "R3, L2, L2, R4, L1, R2, R3, R4, L2, R4, L2, L5, L1, R5, R2, R2, L1, R4, R1, L5, L3, R4, R3, R1, L1, L5, L4, L2, R5, L3, L4, R3, R1, L3, R1, L3, R3, L4, R2, R5, L190, R2, L3, R47, R4, L3, R78, L1, R3, R190, R4, L3, R4, R2, R5, R3, R4, R3, L1, L4, R3, L4, R1, L4, L5, R3, L3, L4, R1, R2, L4, L3, R3, R3, L2, L5, R1, L4, L1, R5, L5, R1, R5, L4, R2, L2, R1, L5, L4, R4, R4, R3, R2, R3, L1, R4, R5, L2, L5, L4, L1, R4, L4, R4, L4, R1, R5, L1, R1, L5, R5, R1, R1, L3, L1, R4, L1, L4, L4, L3, R1, R4, R1, R1, R2, L5, L2, R4, L1, R3, L5, L2, R5, L4, R5, L5, R3, R4, L3, L3, L2, R2, L5, L5, R3, R4, R3, R4, R3, R1"
         finder = DistanceToDestinationFinder(test_input)
-        self.assertEqual(262, finder.get_shortest_path_using_whole_input())
+        self.assertEqual(262, finder.get_shortest_path_from_input())
 
 if __name__ == "__main__":
     unittest.main()
