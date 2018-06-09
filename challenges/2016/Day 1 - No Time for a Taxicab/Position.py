8215c6dbf21496811d907b268a5fbd0d61ce40b0
Phrancis <Phrancis@users.noreply.github.com>
2018-06-09 01:50:36 -0400

    Create Position class

---
+++ challenges/2016/Day 1 - No Time for a Taxicab/Position.py
@@ -0,0 +1,23 @@
+class Position:
+
+    """
+    Represents a position (latitude, longitude) relative to a starting point 
+    where a positive latitude indicates North and negative indicates South,
+    and a positive longitude indicates East and negative indicates West.
+    """
+    
+    def __init__(self, latitude:int, longitude:int) -> None:
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def __eq__(self, other:"Position") -> bool:
+        """
+        Returns whether 2 Position instances are considered equal
+        when using == operator.
+        """
+        return self.latitude == other.latitude and \
+               self.longitude == other.longitude
+
+    def __str__(self) -> str:
+        """Returns a human-readable string representation."""
+        return f"latitude:{self.latitude}, longitude:{self.longitude}"
